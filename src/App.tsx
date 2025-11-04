47eb3914d83a2fdaae88f86051768a4ed19a79c7
Filipe Mai <f.nunesmai19@gmail.com>
2025-11-04 15:52:39 -0300

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { ArrowUpIcon } from "lucide-react";
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +10,13 @@ function App() {
       <p className="read-the-docs text-4xl">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" size="icon" aria-label="Submit">
+          <ArrowUpIcon />
+        </Button>
+      </div>
     </>
   );
 }
